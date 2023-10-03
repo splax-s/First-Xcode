5e0925821f007330acfc483a42a75905a8b2dc6b
Ikhanoba Michael-Shaka <shakaikhanoba@gmail.com>
2023-10-03 14:25:29 +0100

    Initial Commit

---
+++ intro/introApp.swift
@@ -0,0 +1,17 @@
+//
+//  introApp.swift
+//  intro
+//
+//  Created by Michael-Shaka Ikhanoba on 03/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct introApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
